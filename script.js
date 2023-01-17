7f10aa4ddcccc9e073f44ad9f2d3588877017419
Avinasen <115348427+coders2-HCJ@users.noreply.github.com>
2023-01-17 14:07:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,20 +13,20 @@ js.setTheme("ace/theme/light");
 js.session.setMode("ace/mode/javascript");
 js.session.setUseWorker(false);
 function showRes() {
-	result.src = "data:text/html;charset=utf-8," + encodeURIComponent("<!DOCTYPE html><html><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><style>" + css.getValue() + "</style></head><body>" + html.getValue() + "<script>" + js.getValue() + "</script></body></html>");
+  result.src = "data:text/html;charset=utf-8," + encodeURIComponent("<!DOCTYPE html><html><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><style>" + css.getValue() + "</style></head><body>" + html.getValue() + "<script>" + js.getValue() + "<" + "/script></body></html>");
 }
 document.body.onkeyup = showRes;
 showRes();
 function download(filename, text) {
-	var element = document.createElement("a");
-	element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(text));
-	element.setAttribute("download", filename);
-	element.style.display = "none";
-	document.body.appendChild(element);
-	element.click();
-	document.body.removeChild(element);
+  var element = document.createElement("a");
+  element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(text));
+  element.setAttribute("download", filename);
+  element.style.display = "none";
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
 }
 document.getElementById("save").onclick = function() {
-	download("program.html", "<!DOCTYPE html>\n<html>\n<head>\n<title>" + title.value + "</title>\n<meta charset='utf-8'>\n<meta name='viewport' content='width=device-width, initial-scale=1'>\n<style>\n" + css.getValue() + "\n</style>\n</head>\n<body>\n" + html.getValue() + "\n<script>\n" + js.getValue() + "\n</script>\n</body>\n</html>");
+  download("program.html", "<!DOCTYPE html>\n<html>\n<head>\n<title>" + title.value + "</title>\n<meta charset='utf-8'>\n<meta name='viewport' content='width=device-width, initial-scale=1'>\n<style>\n" + css.getValue() + "\n</style>\n</head>\n<body>\n" + html.getValue() + "\n<script>\n" + js.getValue() + "\n<" + "/script>\n</body>\n</html>");
 };
 document.getElementById("reload").onclick = function() {showRes();};
